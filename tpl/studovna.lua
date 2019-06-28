9b16df0d714008b236ec122d228982d2a4475444
Michal Hoftich <michal.h21@gmail.com>
2019-06-28 10:27:32 +0200

    template pro studovnu

---
+++ tpl/studovna.lua
@@ -0,0 +1,10 @@
+codes="data/codes.txt"
+data = "data/studovna-revize.tsv"
+
+
+lokace = "Rett-knihovna"
+statusy = {["Nelze půjčit"]=true}
+lokace = "Rett-studovna" 
+
+tests = {"barcode", "pujceno", "status", "lokace", "zpracovani" , "section"}
+
