2a4702b264dd0516c051bcec51ee07f094307fc5
Michal Hoftich <michal.h21@gmail.com>
2019-06-28 15:51:26 +0200

    zmeny

--- tpl/studovna.lua
+++ tpl/studovna.lua
@@ -6,5 +6,6 @@ lokace = "Rett-knihovna"
 statusy = {["Nelze půjčit"]=true}
 lokace = "Rett-studovna" 
 
+-- testy při načítání čárových kódů
 tests = {"barcode", "pujceno", "status", "lokace", "zpracovani" , "section"}
 
