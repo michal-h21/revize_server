875ab9efb03ee629170a01e4fcfcca3afd45bf53
Michal Hoftich <michal.h21@gmail.com>
2019-07-04 15:41:03 +0200

    skript na nactene poradi

---
+++ nacteneporadi.lua
@@ -0,0 +1,31 @@
+-- get script path
+local path = arg[0]:gsub("[^/]+$", "")
+package.path = path .."?.lua;./?.lua;" .. package.path
+
+local revizeobj = require("revize/revizeobj")
+local config = require("revize/config")
+
+
+local dir = arg[1]:gsub("/$", "")
+
+
+local config_file = dir .. "/config.lua" 
+
+
+local settings = config.load_file(config_file)
+
+local codes = settings.codes or "data/codes.txt"
+local data = settings.data or "data/data.tsv"
+codes = codes:gsub("^data", dir)
+data = data:gsub("^data", dir)
+revizeobj:load_data(data)
+revizeobj:load_codes(codes)
+for i, code in ipairs(revizeobj.codes) do
+  local barcode = code.barcode
+  local section = code.section
+  settings.current_pos = i
+  local messages = revizeobj:run_tests(barcode, section, settings, settings.tests)
+  local record = revizeobj:get_record(barcode) or {}
+  print(barcode, section, table.concat(messages, ","),record.signatura)
+
+end
