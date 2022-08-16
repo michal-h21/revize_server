c839001bf36c2966bd213dbb3880475bdfee7b3a
Michal Hoftich <michal.h21@gmail.com>
2022-08-16 14:21:53 +0200

    správnej status pro studovnu

--- tpl/studovna.lua
+++ tpl/studovna.lua
@@ -4,6 +4,9 @@ data = "data/studovna-revize.tsv"
 
 lokace = "Rett-knihovna"
 statusy = {["Nelze půjčit"]=true}
+statusy = {
+  ["Not for loan"]=true,
+}
 lokace = "Rett-studovna" 
 
 -- testy při načítání čárových kódů
